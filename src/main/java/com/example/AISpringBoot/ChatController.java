eeac9cde105dbb78e6a7d71a547251989adf9f81
IB-Himanshu007 <hibisht98@gmail.com>
2026-02-22 02:22:00 +0530

    Model And CLient usage

--- src/main/java/com/example/AISpringBoot/ChatController.java
+++ src/main/java/com/example/AISpringBoot/ChatController.java
@@ -10,18 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatController {
-	/*
-	 * private ChatClient chatClient;
-	 * 
-	 * public ChatController(ChatClient.Builder chatClientBuilder) {
-	 * this.chatClient= chatClientBuilder.build(); }
-	 */
+	private ChatClient chatClient;
 	@Autowired
 	private OpenAiChatModel chatModel;
+
+	 public ChatController(ChatClient.Builder chatClientBuilder) {
+		 this.chatClient= chatClientBuilder.build(); }
+
 	
-	@GetMapping("/jokes")
-	public String generate(@RequestParam(value="message",defaultValue = "Tell me a Joke non veg") String message) {
+	@GetMapping("/model")
+	public String generate1(@RequestParam(value="message",defaultValue = "Tell me a Joke non veg") String message) {
 		return chatModel.call(message);
 		
+	}
+
+	@GetMapping("/client")
+	public String generate(@RequestParam(value="message",defaultValue = "Tell me a Joke non veg") String message) {
+		return chatClient.prompt(message).call().content();
+
 	}
 }
